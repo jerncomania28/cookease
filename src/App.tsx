9496968f06d805f9f415e894b33177fb991fa22c
jerncomania28 <okonnduesojeremiah@gmail.com>
2023-07-25 02:05:53 +0100

    feat: Initial files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="text-[20px] font-[600] text-blue-300">
+      Coookease Application
+    </div>
+  );
+};
+
+export default App;
